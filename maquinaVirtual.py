7d22d18c20d3a882ff3d633cc2854336b013a46e
rob <robert_rivmtz@hotmail.com>
2015-04-14 14:25:37 -0500

    a

---
+++ maquinaVirtual.py
@@ -0,0 +1,24 @@
+import sys
+from cubo_semantico import *
+
+class maquinaVirtual:
+
+	dirPrincipal = None
+
+	cuboSem = cubo_semantico()
+
+	InstruccionIndex = 0
+
+	cuadruplos = None
+
+	memoria = [[[],[],[]],[[],[],[]],[[],[],[]],[[],[],[]]]
+
+
+	def __init__(self, dirPrincipal:dict, cuadruplos:list):
+		self.dirPrincipal = dirPrincipal
+		self.cuadruplos = cuadruplos
+
+	def empezar(self):
+		while (self.cuadruplos[self.InstruccionIndex[0]] != "END"){
+			
+		}
